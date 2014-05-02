ca1981add2bb95fb02b24aef4abce661ae261bf8
ferrygun <ferrygun@yahoo.com>
2014-05-02 10:20:31 +0800

    Create TravelAgency.js

---
+++ app/store/TravelAgency.js
@@ -0,0 +1,43 @@
+Ext.define("TravelAgency.store.TravelAgency", {
+    extend: "Ext.data.Store",
+    requires: ["Ext.data.proxy.OData",
+			   "TravelAgency.api.Error"
+	],
+    config: {
+		model: "TravelAgency.model.TravelAgency", 
+
+		fields: [
+
+			{ name: "agencynum" },
+			{ name: "NAME" },
+			{ name: "STREET" },
+			{ name: "POSTBOX" },
+			{ name: "POSTCODE" },
+			{ name: "CITY" },
+			{ name: "COUNTRY" },
+			{ name: "REGION" },
+			{ name: "TELEPHONE" },
+			{ name: "URL" },
+			{ name: "LANGU" },
+			{ name: "CURRENCY" },
+			{ name: "mimeType" }
+		],
+
+		proxy: {
+				type: 'odata', 
+				enablePagingParams: false,
+				withCredentials: true,	
+				username: 'P1940517116',
+				password: 'Papamama18',
+				url: "http://ASPSGPLR81BC0G/sapgw/sap/opu/odata/IWFND/RMTSAMPLEFLIGHT/TravelagencyCollection",
+	
+				listeners: {
+					'exception': function (proxy, response, operation) {
+						TravelAgency.api.Error.alert(response);
+					}
+				},
+
+		},
+		autoLoad: true
+	}
+});
